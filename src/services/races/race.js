2037ac00f7f1a81522e2701cf0f9ce52257c77a7
marcy <mm.leitea@outlook.com>
2022-05-23 14:13:05 -0300

    :construction: race building

---
+++ src/services/races/race.js
@@ -0,0 +1,3 @@
+export default (modifiers, abilities, size = 'medio', speed = 9) => {
+	return Object.assign({}, { modifiers, abilities, size, speed })
+}
---
+++ src/tests/races/race.test.js
@@ -0,0 +1,35 @@
+import race from '../../services/races/race.js'
+describe('race factory', () => {
+	it('should return a valid race', () => {
+		const res = race({ for: 2 }, [], 'medio', 9)
+
+		expect(res).toEqual({
+			modifiers: {
+				for: 2,
+			},
+			abilities: [],
+			size: 'medio',
+			speed: 9,
+		})
+	})
+	it.only('should throw if given invalid attribute', () => {
+		expect(() => {
+			race({ wei: 2 }, [], 'medio', 9)
+		}).toThrow()
+	})
+	it('should throw if given invalid size', () => {
+		expect(() => {
+			race({ for: 2 }, [], 'BIG', 3)
+		}).toThrow()
+	})
+	it('should throw if given invalid speed', () => {
+		expect(() => {
+			race({ for: 2 }, [], 'medio', 'rapido')
+		}).toThrow()
+	})
+	it.skip('should throw if given invalid ability', () => {
+		expect(() => {
+			race({ for: 2 }, ['pro player'], 'medio', 6)
+		}).toThrow()
+	})
+})
