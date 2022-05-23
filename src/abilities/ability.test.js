cf1874d944ed263a72e35a49dadac80c50b66a2f
marcy <mm.leitea@outlook.com>
2022-05-23 16:37:26 -0300

    :white_check_mark: update ability tests strucutes

--- src/abilities/ability.test.js
+++ src/abilities/ability.test.js
@@ -1,30 +1,22 @@
 import ability from './ability.js'
 describe('abilities factory', () => {
+	const applyInSkill = (sheet) => {}
 	it('should return a valid ability', () => {
-		const funcs = {
-			applyOnSkill: (sheet) => {},
-		}
-
-		const compareAbility = Object.assign({ name: 'i am a ability' }, funcs)
-
-		const res = ability('i am a ability', funcs)
-		expect(res).toEqual(compareAbility)
+		const res = ability('i am a ability', { applyInSkill })
+		console.log(res)
+		expect(res.name).toBe('i am a ability')
+		expect(res).toHaveProperty('applyInSkill')
 	})
 	it('should return a valid ability ignoring not used functions', () => {
-		const funcs = {
-			applyOnSkill: (sheet) => {},
-			applyOnHugsAndKisses: (sheet) => {},
-		}
-		let compareFuncs = Object.assign({}, funcs)
-		compareFuncs.applyOnHugsAndKisses = undefined
+		const applyOnHugsAndKisses = (sheet) => {}
 
-		const compareAbility = Object.assign(
-			{ name: 'i am a ability' },
-			compareFuncs
-		)
-
-		const res = ability('i am a ability', funcs)
-		expect(res).toEqual(compareAbility)
+		const res = ability('i am a ability', {
+			applyInSkill,
+			applyOnHugsAndKisses,
+		})
+		expect(res.name).toBe('i am a ability')
+		expect(res).toHaveProperty('applyInSkill')
+		expect(res).not.toHaveProperty('applyOnHugsAndKisses')
 	})
 	it('should return with name lowercase', () => {
 		const res = ability('HOW DO I STOP SCREAMING')
@@ -43,14 +35,14 @@ describe('abilities factory', () => {
 	it('should throw with function with no args', () => {
 		expect(() => {
 			ability('no args!', {
-				applyOnSkill: () => {},
+				applyInSkill: () => {},
 			})
 		}).toThrowError('ability error: functions must recieve only one argument')
 	})
 	it('should throw with function with more than one arg', () => {
 		expect(() => {
 			ability('to much args!', {
-				applyOnSkill: (sheet, something) => {},
+				applyInSkill: (sheet, something) => {},
 			})
 		}).toThrowError('ability error: functions must recieve only one argument')
 	})
