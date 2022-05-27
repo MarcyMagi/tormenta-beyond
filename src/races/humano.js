1187884dfb3e0990ea322f71b467ffc93d33c299
marcy <mm.leitea@outlook.com>
2022-05-26 21:43:29 -0300

    :label: add humano race

---
+++ src/races/humano.js
@@ -0,0 +1,17 @@
+export default {
+	name: 'humano',
+	description: "hi, i'm a human",
+	modifiers: {
+		for: 0,
+		des: 0,
+		con: 0,
+		int: 0,
+		sab: 0,
+		car: 0,
+	},
+	abilities: ['versátil'],
+	customModifiers: {
+		max: 3,
+		value: 2,
+	},
+}
