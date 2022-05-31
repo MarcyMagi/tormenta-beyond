8adab245aaa5d5efc946741aab7ce96508374e41
marcy <mm.leitea@outlook.com>
2022-05-31 16:15:01 -0300

    :bug: add abilities to race properties

--- src/races/basics.js
+++ src/races/basics.js
@@ -1,8 +1,9 @@
 export default (state) => {
 	const name = state.name
 	const description = state.description
+	const abilities = state.abilities
 	if (name === '') {
 		throw new Error('race basics error: name cannot be empty string')
 	}
-	return Object.assign({}, { name, description })
+	return Object.assign({}, { name, description, abilities })
 }
