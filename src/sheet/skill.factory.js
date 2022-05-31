1d2fe87dfdef8e2faf02ad2e77896906b52e6156
marcy <mm.leitea@outlook.com>
2022-05-31 14:30:04 -0300

    :wrench: replace attributes with config defaultattributes

--- src/sheet/skill.factory.js
+++ src/sheet/skill.factory.js
@@ -1,9 +1,9 @@
-const attributes = ['for', 'des', 'con', 'int', 'sab', 'car']
+import { defaultAttributes } from '../config/sheet-defaults.js'
 export default (name, attribute, config = {}) => {
 	if (name === '') {
 		throw new Error('skill error: name cannot be empty string')
 	}
-	if (!attributes.includes(attribute)) {
+	if (!defaultAttributes.includes(attribute)) {
 		throw new Error('skill error: attribute must be valid')
 	}
 	const armorPenalty = config.armorPenalty ? config.armorPenalty : false
