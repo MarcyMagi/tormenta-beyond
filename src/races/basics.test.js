310e67b8e1b1fe2e874689220d148191b5996ed5
marcy <mm.leitea@outlook.com>
2022-06-01 10:56:38 -0300

    :white_check_mark: add abilities, size and speed to test

--- src/races/basics.test.js
+++ src/races/basics.test.js
@@ -4,9 +4,14 @@ describe('race basics', () => {
 		const basics = raceBasics({
 			name: 'humano',
 			description: 'o povo mais numeroso de arton',
+			abilities: ['versátil'],
+			size: 'grande',
+			speed: 12,
 		})
 		expect(basics.name).toBe('humano')
 		expect(basics.description).toBe('o povo mais numeroso de arton')
+		expect(basics.size).toBe('médio')
+		expect(basics.speed).toBe(9)
 	})
 	it('should ignore weird value', () => {
 		const basics = raceBasics({
