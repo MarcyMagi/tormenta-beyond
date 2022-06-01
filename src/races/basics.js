74bc70f6914f0cb3bddcc419621886b5648f8760
marcy <mm.leitea@outlook.com>
2022-06-01 10:56:16 -0300

    :bug: add default size and speed to race basics

--- src/races/basics.js
+++ src/races/basics.js
@@ -2,8 +2,10 @@ export default (state) => {
 	const name = state.name
 	const description = state.description
 	const abilities = state.abilities
+	const size = 'médio'
+	const speed = 9
 	if (name === '') {
 		throw new Error('race basics error: name cannot be empty string')
 	}
-	return Object.assign({}, { name, description, abilities })
+	return Object.assign({}, { name, description, abilities, size, speed })
 }
