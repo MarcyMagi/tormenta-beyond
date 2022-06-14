bc6a8d65f8461f3556cfcca540ab640c539653ec
marcy <mm.leitea@outlook.com>
2022-06-14 12:58:10 -0300

    :truck: rename loaderUtils > loader-utils + freeze

--- src/utils/loader-utils.js
+++ src/utils/loader-utils.js
@@ -1,7 +1,7 @@
-import _attributesUtils from './attributesUtils.js'
+import _attributesUtils from './attributes-utils.js'
 import _choose from './choose.js'
 export default (config) => {
 	const attributesUtils = _attributesUtils(config.defaultAttributes)
 	const choose = _choose
-	return Object.assign({}, { attributesUtils, choose })
+	return Object.freeze({ attributesUtils, choose })
 }
