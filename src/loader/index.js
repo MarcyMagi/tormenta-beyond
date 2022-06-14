95f63e9438cac616398dbcc2f1bcd45bfc5dc81f
marcy <mm.leitea@outlook.com>
2022-06-14 18:34:45 -0300

    :sparkles: add skill suport + skill acrobacia

--- src/loader/index.js
+++ src/loader/index.js
@@ -1,9 +1,19 @@
 import folderLoader from './folder-loader.js'
-import loaderUtils from '../utils/loader-utils.js'
+import basicUtils from '../utils/basic-utils.js'
 import * as config from '../config.js'
 
+const convertUtil = (obj) => {
+	return {
+		list: () => {
+			return Object.keys(obj)
+		},
+	}
+}
+
 export default async () => {
-	const utils = loaderUtils(config)
+	const utils = basicUtils(config)
+	const skills = await folderLoader('skills', utils)
+	utils.skills = convertUtil(skills)
 	const races = await folderLoader('races', utils)
 	return Object.freeze({ races })
 }
---
+++ src/skills/acrobacia.js
@@ -0,0 +1,8 @@
+export default (utils) => {
+	return {
+		name: 'acrobacia',
+		attribute: 'des',
+		trained: false,
+		penality: true,
+	}
+}
