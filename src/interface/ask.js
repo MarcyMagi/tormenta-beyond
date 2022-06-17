bb21b88369df1f6b495b5a61575b7cd3a6d5838e
marcy <mm.leitea@outlook.com>
2022-06-17 11:46:19 -0300

    :recycle: change how prompt build questions

--- src/interface/ask.js
+++ src/interface/ask.js
@@ -1,26 +1,35 @@
-export default async (obj, prompt) => {
+const ask = async (obj, prompt) => {
 	for (const [key, value] of Object.entries(obj)) {
 		if (typeof value === 'object') {
 			if (value.choose) {
+				const askArr = []
+				for (let i = 0; i < value.specs.quantity; i++) {
+					askArr.push({
+						type: 'list',
+						name: 'arg_' + i,
+						message: value.specs.label + ` [${i + 1}]`,
+						choices: value.specs.options,
+					})
+				}
 				for (let i = 0; i < 100; i++) {
-					const { res } = await prompt([
-						{
-							type: 'list',
-							name: 'res',
-							message: value.specs.label,
-							choices: value.specs.options,
-						},
-					])
+					const resObj = await prompt(askArr)
+					const resArr = Object.values(resObj)
 					try {
-						const chosen = value.choose(...res)
+						console.log(resArr)
+						const chosen = value.choose(...resArr)
 						obj[key] = chosen
 						break
-					} catch {
+					} catch (e) {
+						console.log(e)
 						continue
 					}
 				}
+			} else {
+				obj[key] = await ask(value, prompt)
 			}
 		}
 	}
 	return obj
 }
+
+export default ask
--- src/interface/ask.test.js
+++ src/interface/ask.test.js
@@ -2,27 +2,29 @@ import { jest } from '@jest/globals'
 import choose from '../utils/choose'
 import ask from './ask'
 describe('interface ask details', () => {
-	const config = {
-		id: 'id',
-		display: 'Something',
-		anotherThing: choose('question!', ['a', 1, false], 2, (chosen) => ({
-			first: chosen[0],
-			second: chosen[1],
-		})),
-	}
 	it('should return correct', async () => {
-		const fakePrompt = jest
-			.fn(async () => {})
-			.mockReturnValueOnce({ res: ['a', false] })
-		const newConfig = Object.assign({}, config)
-		console.log(newConfig)
-		const configured = await ask(newConfig, fakePrompt)
+		const config = {
+			id: 'id',
+			display: 'Something',
+			anotherThing: choose('question!', ['a', 1, false], 2, (chosen) => ({
+				first: chosen[0],
+				second: chosen[1],
+			})),
+		}
+		const fakePrompt = jest.fn().mockReturnValueOnce({ 1: 'a', 2: false })
+		const configured = await ask(config, fakePrompt)
 		expect(fakePrompt.mock.calls.length).toBe(1)
 		expect(fakePrompt.mock.calls[0][0]).toEqual([
 			{
 				type: 'list',
-				name: 'res',
-				message: 'question!',
+				name: 'arg_0',
+				message: 'question! [1]',
+				choices: ['a', 1, false],
+			},
+			{
+				type: 'list',
+				name: 'arg_1',
+				message: 'question! [2]',
 				choices: ['a', 1, false],
 			},
 		])
@@ -34,17 +36,65 @@ describe('interface ask details', () => {
 		})
 	})
 	it('should try again if invalid choice', async () => {
+		const config = {
+			id: 'id',
+			display: 'Something',
+			anotherThing: choose('question!', ['a', 1, false], 2, (chosen) => ({
+				first: chosen[0],
+				second: chosen[1],
+			})),
+		}
 		const fakePrompt = jest
-			.fn(async () => {})
-			.mockReturnValueOnce({ res: ['a', true] })
-			.mockReturnValueOnce({ res: ['a', 1] })
-		const newConfig = Object.assign({}, config)
-		const configured = await ask(newConfig, fakePrompt)
+			.fn()
+			.mockReturnValueOnce({ 1: 'a', 2: true })
+			.mockReturnValueOnce({ 1: 'a', 2: 1 })
+		const configured = await ask(config, fakePrompt)
 		expect(fakePrompt.mock.calls.length).toBe(2)
 		expect(configured.anotherThing).toEqual({
 			first: 'a',
 			second: 1,
 		})
 	})
-	it('should work recursive', () => {})
+	it('should work recursively', async () => {
+		const config = {
+			id: 'id',
+			display: 'hello',
+			choose1: choose('question 1!', ['a'], 1, (chosen) => chosen),
+			thing: {
+				choose2: choose('question 2!', ['b'], 1, (chosen) => chosen),
+				anotherThing: {
+					choose3: choose('question 3!', ['c'], 1, (chosen) => chosen),
+				},
+			},
+		}
+		const fakePrompt = jest
+			.fn()
+			.mockReturnValueOnce({ 1: 'a' })
+			.mockReturnValueOnce({ 1: 'b' })
+			.mockReturnValueOnce({ 1: 'c' })
+		const configured = await ask(config, fakePrompt)
+		expect(configured.choose1).toEqual(['a'])
+		expect(configured.thing.choose2).toEqual(['b'])
+		expect(configured.thing.anotherThing.choose3).toEqual(['c'])
+	})
+	it('should work in array', async () => {
+		const config = {
+			id: 'id',
+			display: 'hello',
+			arr: [
+				choose('question 1!', ['a'], 1, (chosen) => chosen),
+				choose('question 2!', ['b'], 1, (chosen) => chosen),
+				choose('question 3!', ['c'], 1, (chosen) => chosen),
+			],
+		}
+		const fakePrompt = jest
+			.fn()
+			.mockReturnValueOnce({ 1: 'a' })
+			.mockReturnValueOnce({ 1: 'b' })
+			.mockReturnValueOnce({ 1: 'c' })
+		const configured = await ask(config, fakePrompt)
+		expect(configured.arr[0]).toEqual(['a'])
+		expect(configured.arr[1]).toEqual(['b'])
+		expect(configured.arr[2]).toEqual(['c'])
+	})
 })
