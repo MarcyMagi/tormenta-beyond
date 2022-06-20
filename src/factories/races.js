78421b90252a7f31a462eb901dc1485d0fb1c512
marcy <mm.leitea@outlook.com>
2022-06-20 12:25:29 -0300

    :mute: remove log race name

--- src/factories/races.js
+++ src/factories/races.js
@@ -1,7 +1,6 @@
 import { attributes } from '../cache/index.js'
 
 export default (name, modifiers, abilities) => {
-	console.log(name)
 	name = name.toLowerCase()
 	modifiers = attributes.filter(modifiers)
 	return Object.freeze({ name, modifiers, abilities })
