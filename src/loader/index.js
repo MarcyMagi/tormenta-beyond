a37773edfbc5b3d0b65e684e134820c53809f9ff
marcy <mm.leitea@outlook.com>
2022-06-20 19:40:23 -0300

    :recycle: change how data is loaded

--- src/loader/index.js
+++ src/loader/index.js
@@ -1,15 +1,27 @@
 import attributesLoader from './attributes.js'
 import utilsLoader from './utils.js'
 import choose from '../utils/choose.js'
-import * as config from '../config.js'
 import loadFolder from './load-folder.js'
 
-const utils = { choose }
-export const attributes = attributesLoader(config.attributes)
-utils.attributes = attributes
-export const skills = await loadFolder('skills', utils)
-utils.skills = utilsLoader(skills)
-export const powers = await loadFolder('powers', utils)
-utils.powers = utilsLoader(powers)
+export default async (config) => {
+	const utils = {
+		choose,
+	}
+	utils.attributes = attributesLoader(config.attributes)
 
-export const races = await loadFolder('races', utils)
+	const skills = await loadFolder('skills', utils, ['description'])
+	utils.skills = utilsLoader(skills)
+
+	const powers = await loadFolder('powers', utils, ['description'])
+	utils.powers = utilsLoader(powers)
+
+	const races = await loadFolder('races', utils, ['description', 'tale'])
+	utils.races = utilsLoader(races)
+
+	return {
+		utils,
+		skills,
+		powers,
+		races,
+	}
+}
--- src/loader/load-folder.js
+++ src/loader/load-folder.js
@@ -3,15 +3,18 @@ import fs from 'fs'
 
 const dataDir = path.resolve('./src/tormentaData')
 
-export default async (folderName, utils) => {
+export default async (folderName, utils, ignoreKeys = []) => {
 	const folderPath = `${dataDir}/${folderName}`
 	const obj = {}
 	const folderArr = fs.readdirSync(folderPath)
 	for await (const fileName of folderArr) {
 		const defaultData = await import(`${folderPath}/${fileName}`)
 		const data = defaultData.default(utils)
+		for (const ignore of ignoreKeys) {
+			delete data[ignore]
+		}
 		const id = data.id.toLowerCase()
-		obj[id] = Object.freeze(data)
+		obj[id] = data
 	}
 	return obj
 }
