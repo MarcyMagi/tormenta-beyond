5d059a7182240862def9d4c027dd5176db964f95
marcy <mm.leitea@outlook.com>
2022-07-23 19:34:02 -0300

    :construction: hp wip

--- src/sheet/hp.factory.js
+++ src/sheet/hp.factory.js
@@ -1,1 +1,23 @@
-import PointBehavior from './composition/point-behavior.factory.js'
+import AdderData from './composition/adder-data.factory'
+export default (sheet, keyAttribute) => {
+	const dict = () => {
+		const modifier = sheet.attributes.modifiers()[keyAttribute]
+		const classList = Object.entries(sheet.classes.list)
+		const adderDict = classList.reduce((dict, [key, obj]) => {
+			for (let i = 1; i <= obj.level(); i++) {
+				const isFirst = obj.isFirst
+				if (isFirst) {
+					const adderData = AdderData()
+					adderData.set('level', obj.level1)
+					adderData.set('modifier', modifier)
+					dict[`first${key}[${i}]`] = adderData
+					isFirst = false
+				}
+			}
+			return dict
+		}, {})
+		console.log(adderDict)
+	}
+	dict()
+	return {}
+}
--- src/sheet/hp.test.js
+++ src/sheet/hp.test.js
@@ -3,11 +3,71 @@ import { jest } from '@jest/globals'
 describe('sheet hp factory', () => {
 	let hp
 	let sheet = {
+		attributes: {
+			modifiers: jest.fn().mockReturnValue({
+				con: 1,
+				car: 4,
+			}),
+		},
 		classes: {
-			totalLevel: jest.fn().mockReturnValue(9),
+			list: {
+				arcanista: {
+					level1: 8,
+					levelup: 2,
+					isFirst: true,
+					level: jest.fn().mockReturnValue(2),
+				},
+				barbaro: {
+					level1: 24,
+					levelup: 6,
+					level: jest.fn().mockReturnValue(1),
+				},
+			},
+			totalLevel: jest.fn().mockReturnValue(3),
 		},
 	}
 	beforeEach(() => {
-		hp = Hp(sheet)
+		hp = Hp(sheet, 'con')
+	})
+
+	it.only('should get max correctly', () => {
+		const max = hp.max()
+		expect(max).toBe(19)
+	})
+
+	it('should get max correctly with negative levelUp', () => {
+		sheet.attributes.modifiers.mockReturnValueOnce({ con: -6, car: 3 })
+		const max = hp.max()
+		expect(max).toBe(4)
+	})
+	it('should get current value correctly', () => {
+		const cur = hp.current()
+		expect(cur).toBe(19)
+	})
+	it('should apply changes to current', () => {
+		hp.apply(-2)
+		const cur = hp.current()
+		expect(cur).toBe(17)
+		const max = hp.max()
+		expect(max).toBe(19)
+	})
+	it('should not add more than max', () => {
+		hp.apply(3)
+		const cur = hp.current()
+		const max = hp.max()
+		expect(cur > max).toBe(false)
+	})
+	it('should not remove more than -max', () => {
+		hp.apply(-100)
+		const cur = hp.current()
+		const max = hp.max()
+		expect(cur < -max).toBe(false)
+	})
+	it.skip('should add fix value to max', () => {
+		hp.setFix('fix', 3)
+		const max = hp.max()
+		expect(max).toBe(22)
+		const cur = hp.cur()
+		expect(cur).toBe(19)
 	})
 })
