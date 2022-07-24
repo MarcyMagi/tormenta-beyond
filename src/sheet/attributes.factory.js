07e015f2be96c1722e38f5c60979ce196dbe7ffc
marcy <mm.leitea@outlook.com>
2022-07-24 18:45:26 -0300

    :art: create an init function and rename some stuff

--- src/sheet/attributes.factory.js
+++ src/sheet/attributes.factory.js
@@ -3,35 +3,37 @@ import AdderData from './composition/adder-data.factory.js'
 import { attributes as attributesLabels } from '../config.js'
 
 export default (baseValues) => {
-	const attributesUtils = AttributesUtils(attributesLabels)
-	const _baseData = attributesUtils.filter(baseValues, true)
+	const _attributesUtils = AttributesUtils(attributesLabels)
 	const _dict = {}
-	for (const [key, value] of Object.entries(_baseData)) {
-		const newAttribute = AdderData()
-		newAttribute.set('base', value)
-		_dict[key] = newAttribute
+	const init = () => {
+		const _baseData = _attributesUtils.filter(baseValues, true)
+		for (const [key, value] of Object.entries(_baseData)) {
+			const newAttribute = AdderData()
+			newAttribute.set('base', value)
+			_dict[key] = newAttribute
+		}
 	}
 	const values = () => {
-		return attributesLabels.reduce((prev, cur) => {
-			prev[cur] = _dict[cur].calculate()
-			return prev
+		return attributesLabels.reduce((obj, value) => {
+			obj[value] = _dict[value].calculate()
+			return obj
 		}, {})
 	}
 	const modifiers = () => {
 		const entries = Object.entries(values())
-		return entries.reduce((prev, [key, value]) => {
-			prev[key] = Math.floor((value - 10) / 2)
-			return prev
+		return entries.reduce((obj, [key, value]) => {
+			obj[key] = Math.floor((value - 10) / 2)
+			return obj
 		}, {})
 	}
 	const meta = () => {
-		return attributesLabels.reduce((prev, cur) => {
-			prev[cur] = _dict[cur].dict()
-			return prev
+		return attributesLabels.reduce((obj, value) => {
+			obj[value] = _dict[value].dict()
+			return obj
 		}, {})
 	}
 	const setOther = (label, objAttributes) => {
-		const otherValues = attributesUtils.filter(objAttributes)
+		const otherValues = _attributesUtils.filter(objAttributes)
 		for (const [key, value] of Object.entries(otherValues)) {
 			_dict[key].set(label, value)
 		}
@@ -41,5 +43,6 @@ export default (baseValues) => {
 			_dict[attributeLabel].remove(label)
 		}
 	}
+	init()
 	return Object.freeze({ values, modifiers, meta, setOther, removeOther })
 }
